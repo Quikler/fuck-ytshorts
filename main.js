eccaeac8a86311d53e80902c52027a21eeae59a5
Quikler <romuchbinyk228@gmail.com>
2025-09-20 11:10:09 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+(async () => {
+  // link to this amazing function: https://stackoverflow.com/a/61511955/23143871
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(() => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+
+      // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+
+  function removeElement(elem) {
+    console.log("fuck you:", elem);
+    const elemParent = elem.parentNode;
+    elemParent.removeChild(elem);
+  }
+
+  // Remove initially
+
+  // Slide side bar Shorts button
+  waitForElm('a[title="Shorts"][id="endpoint"][class="yt-simple-endpoint style-scope ytd-guide-entry-renderer"]')
+    .then(e => removeElement(e.parentNode));
+
+  // Side bar Short button element
+  waitForElm('ytd-mini-guide-entry-renderer[aria-label="Shorts"]')
+    .then(e => removeElement(e));
+
+  // listen for youtube short shit
+  while (true) {
+    // grid-shelf-view-model
+
+    // selectors to track for 
+    // - '.ytGridShelfViewModelHost' - shorts group class
+    // - 'ytm-shorts-lockup-view-model' - shorts video element
+
+    // TODO: remove initially ytd-mini-guide-entry-renderer[aria-label="Shorts"]
+    const elm = await waitForElm('.ytGridShelfViewModelHost, ytm-shorts-lockup-view-model');
+    removeElement(elm);
+  }
+})();
